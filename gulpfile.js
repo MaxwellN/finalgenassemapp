0235d4613b41575d9219132521c258f7224b33d0
Maxwell Narten <maxwellnarten@maxwells-mini.int.us.randstad.com>
2016-05-26 13:42:13 -0400

    gulp files updated, ready to get coding

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 "use-strict";
 
 var gulp = require('gulp');
+var connect = require('gulp-connect');
+var open = require('gulp-open');
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
 
 
 var config = {
+  port: 5000,
+  baseUrl: 'http://localhost',
   paths: {
     html: './src/*.html',
     js: './src/scripts/*.js',
@@ -14,21 +18,38 @@ var config = {
   }
 };
 
+gulp.task('open', ['connect'], function() {
+  gulp.src('dist/index.html')
+    .pipe(open({ uri:config.baseUrl + ':' + config.port + '/'}));
+});
 
 gulp.task('js', function(){
   return gulp.src(config.paths.js)
-    .pipe(gulp.dest(config.paths.dist + '/scripts'));
+    .pipe(gulp.dest(config.paths.dist + '/scripts'))
+    .pipe(connect.reload());
 });
 
 gulp.task('html', function(){
   return gulp.src(config.paths.html)
-    .pipe(gulp.dest(config.paths.dist));
+    .pipe(gulp.dest(config.paths.dist))
+    .pipe(connect.reload());
 });
 
 gulp.task('sass', function(){
   return gulp.src(config.paths.sass)
     .pipe(sass().on('error', sass.logError))
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(gulp.dest(config.paths.dist + '/styles'))
+    .pipe(connect.reload());
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    root: ['dist'],
+    port: config.port,
+    base: config.baseUrl,
+    livereload: true
+  })
 });
 
 
@@ -42,4 +63,4 @@ gulp.task('watch', function(){
 })
 
 
-gulp.task('default', ['watch', 'compress']);
+gulp.task('default', ['open', 'watch']);
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,2 +1,16 @@
-console.log("i loaded!");
-console.log("more load");
+$(document).ready(function() {
+  $('#search-button').click(function() {
+    $.getJSON('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=00ef8b5d09f5a093d587657ec6fa88c3&t&format=json&nojsoncallback=1&api_sig=9527e11a4431a39fe5d05c6b1932b4a1',
+    {
+      tags: 'dog',
+      tagmode: 'any',
+      format: 'json'
+    },
+    function(data) {
+      $.each(data.items, function(i, item) {
+        $("<img/>").attr('src', item.media.m).appendTo('#images');
+        if(i == 3)  return false;
+      });
+    });
+  });
+});
