19adbce2b06ecc19930c91016754954e0bea9c36
Maxwell Narten <maxwellnarten@maxwells-mini.int.us.randstad.com>
2016-05-27 12:46:40 -0400

    updated js, but not working...because I'm at work?

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,16 +1,36 @@
 $(document).ready(function() {
-  $('#search-button').click(function() {
-    $.getJSON('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=00ef8b5d09f5a093d587657ec6fa88c3&t&format=json&nojsoncallback=1&api_sig=9527e11a4431a39fe5d05c6b1932b4a1',
-    {
-      tags: 'dog',
-      tagmode: 'any',
-      format: 'json'
-    },
-    function(data) {
-      $.each(data.items, function(i, item) {
-        $("<img/>").attr('src', item.media.m).appendTo('#images');
-        if(i == 3)  return false;
-      });
-    });
+
+
+  var searchButton = $('#searchButton');
+
+  searchButton.on('click', search);
+  $('#searchField').on('keyup', function(enter) {
+    if (enter.keyCode === 13) {
+      search();
+    };
   });
 });
+
+
+
+function search(){
+  var searchField = $('#searchField');
+  var newSearch = "";
+  newSearch = (searchField.val() ? searchField.val():alert('Give me a topic!!!'));
+  $.ajax({
+    url: 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=3057bbe793041f44719b1cf3dd4f4748&per_page=30&format=json&nojsoncallback=1&tags=' + newSearch,
+    type: 'GET',
+    success: function(data) {
+      $('#container').html('');
+      $(data.photos.photo).each(function(index, value) {
+        var imageUrl = 'https://farm' + value.farm + '.staticflickr.com/' + value.server + '/' + value.id + '_' + value.secret + '_b.jpg';
+
+        console.log(imageUrl);
+
+        $('<img src="' + imageUrl + '" alt="" />').appendTo($('#container'));
+
+      });
+              console.log(data);
+    }
+  });
+};
